2770e442723c5bec34e2794e5dbc7fe5c90d8ef3
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-08-26 16:07:13 -0700

    solve R505 div1+2 A. Doggo Recoloring

---
+++ solutions/DoggoRecoloring.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cerr
+#define TRACE true
+#define dump(x) if(TRACE) { cerr << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+
+/*
+ 
+ 8/19/2018
+ 
+ 6:35-6:48 AC (1WA for N=1)
+ 
+ Editorials:
+  - http://codeforces.com/blog/entry/61323
+  - http://betrue12.hateblo.jp/entry/2018/08/20/212528
+  - https://twitter.com/satanic0258/status/1031208997158772737
+  - https://twitter.com/Yazaten/status/1031208986035384320
+  - https://togetter.com/li/1258285
+ 
+ */
+
+// 6:42 1WA
+// 6:48 submit
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG x.cpp && ./a.out
+//const int MAX_N=1e5+1;
+int N;
+string S;
+int C[26]={};
+bool solve() {
+  ZERO(C);
+  if(N==1) return true;
+  REP(i,N) C[S[i]-'a']++;
+  bool ng=true;
+  REP(i,26) if(C[i]>0) ng&=(C[i]==1);
+  return !ng;
+}
+
+string yes="Yes",no="No";
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  
+  cin>>N>>S;
+  cout<<(solve()?yes:no)<<endl;
+  return 0;
+}
