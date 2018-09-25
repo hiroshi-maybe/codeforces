56f766ff81f3f691ae6b67803ed0be98603dfef3
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-09-24 20:39:47 -0700

    solve ER51 B. Relatively Prime Pairs

---
+++ solutions/RelativelyPrimePairs.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cerr
+#define TRACE true
+#define dump(x) if(TRACE) { cerr << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+
+/*
+ 
+ 9/20/2018
+ 
+ 8:06-8:16 AC
+ 
+ http://codeforces.com/blog/entry/61969
+ http://betrue12.hateblo.jp/entry/2018/09/23/191155
+ 
+ */
+
+// 8:16 submit
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG x.cpp && ./a.out
+//const int MAX_N=3e5+1;
+LL L,R;
+//LL A[MAX_N];
+LL gcd(LL a, LL b) { return b==0?a:gcd(b,a%b); }
+const string yes="YES",no="NO";
+void solve() {
+  cout<<yes<<endl;
+  for(LL i=L; i<=R; i+=2) {
+    cout<<i<<" "<<i+1<<endl;
+//    assert(gcd(i,i+1)==1);
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  
+  cin>>L>>R;
+  solve();
+  return 0;
+}
