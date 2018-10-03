250ce04d5c638e54a03ac44e3956c0ab527c1f55
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-10-02 17:40:13 -0700

    solve R512 div2 C. Vasya and Golden Ticket

---
+++ solutions/VasyaAndGoldenTicket.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cerr
+#define TRACE true
+#define dump(x) if(TRACE) { cerr << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+
+/*
+ 
+ 9/23/2018 R512 div2
+ 
+ 6:15-7:09 AC
+ 
+ http://codeforces.com/blog/entry/62013
+ 
+ */
+
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG x.cpp && ./a.out
+//const int MAX_N=1e3+1;
+int N;
+string S;
+
+bool solve() {
+  FOR(l,1,N) {
+    int a=0; REP(i,l)a+=S[i]-'0';
+    int cur=0,i=l,cnt=1;
+    for(;i<N;++i) {
+      cur+=S[i]-'0';
+      if(cur==a) cur=0,cnt++;
+      else if(cur>a) {
+        break;
+      }
+    }
+    if(i==N&&cur==0&&cnt>1) {
+      //      dump2(l,a);
+      return true;
+    }
+  }
+  return false;
+}
+
+const string yes="YES",no="NO";
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  
+  cin>>N>>S;
+  cout<<(solve()?yes:no)<<endl;
+  
+  return 0;
+}
