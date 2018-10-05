9352ad04905d5bbd60d9e7fb6b531725839a3e03
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-10-04 23:53:40 -0700

    solve R513 div1+div2 D. Social Circles

---
+++ solutions/SocialCircles.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cerr
+#define TRACE true
+#define dump(x) if(TRACE) { cerr << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+
+/*
+ 
+ 10/4/2018
+ 
+ 22:55-23:45 AC
+ 
+ Build bipartite graph with N edges
+ Look at maximal L and maximal R. Pair them with cost max(R,L) and remove them.
+ This gives optimal result.
+ 
+ L R
+ ----
+ a c
+ b d
+ 
+ a<=b, c<=d
+ 
+ 1) a<=b<=c<=d
+ 
+ G(=): max(b,d)+max(a,c)=d+c
+ G(X): max(a,d)+max(b,c)=d+c
+ 
+ G(=)=G(X)
+ 
+ 2) a<=c<=b<=d
+
+ G(=): max(b,d)+max(a,c)=d+c
+ G(X): max(a,d)+max(b,c)=d+b
+ 
+ G(=)<=G(X)
+
+ Thus G(=) is always optimal
+ 
+ */
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG x.cpp && ./a.out
+const int MAX_N=1e5+1;
+LL L[MAX_N],R[MAX_N];
+int N;
+LL solve() {
+  sort(L,L+N),sort(R,R+N);
+  LL res=N;
+  REP(i,N) res+=max(L[i],R[i]);
+  return res;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  
+  cin>>N;
+  REP(i,N) cin>>L[i]>>R[i];
+  cout<<solve()<<endl;
+  
+  return 0;
+}
