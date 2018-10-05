47d430b6fd79394c962004009650a888237df900
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-10-04 22:18:08 -0700

    solve R513 div1+div2 B. Maximum Sum of Digits

---
+++ solutions/MaximumSumOfDigits.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cerr
+#define TRACE true
+#define dump(x) if(TRACE) { cerr << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+
+/*
+ 
+ 10/4/2018
+ 
+ 21:40-22:17 AC
+ 
+ */
+
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG x.cpp && ./a.out
+LL N;
+
+int S(LL n) {
+  int res=0;
+  while(n>0) res+=n%10,n/=10;
+  return res;
+}
+LL solve() {
+  if(N==0) return 0;
+  LL P10[14]; P10[0]=1; REP(i,13) P10[i+1]=P10[i]*10LL;
+  int i=0;
+  while(P10[i+1]-1<=N) ++i;
+  LL res=i*9;
+  N-=P10[i]-1;
+  res+=S(N);
+  return res;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  
+  cin>>N;
+  cout<<solve()<<endl;
+  
+  return 0;
+}
