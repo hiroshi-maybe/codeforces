80c1a04df9cecff085bafcb78ef0ee8398bcfc41
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-10-13 14:47:18 -0700

    solve Lyft L5 Challenge C. Permutation Game

---
+++ solutions/PermutationGame.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+#include <iomanip>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cerr
+#define TRACE true
+#define dump(x) if(TRACE) { cerr << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+/*
+ 
+ 10/7/2018 Lyft Level 5 Challenge 2018 - Elimination Round
+ 
+ 10:26-10:47 AC
+ 
+ https://codeforces.com/blog/entry/62287
+ 
+ */
+// 10:47 submit
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG x.cpp && ./a.out
+const int MAX_N=1e5+1;
+int A[MAX_N];
+int N;
+int memo[MAX_N];
+
+int f(int p) {
+  int &res=memo[p];
+  if(res>=0) return res;
+  res=0;
+  for(int i=p-A[p]; i>=0; i-=A[p]) {
+    if(A[i]>A[p]) res|=!f(i);
+  }
+  for(int i=p+A[p]; i<N; i+=A[p]) if(A[i]>A[p]) res|=!f(i);
+  return res;
+}
+
+string solve() {
+  MINUS(memo);
+  string res(N,'?');
+  REP(i,N) res[i]=f(i)==0?'B':'A';
+  return res;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  
+  cin>>N;
+  REP(i,N) cin>>A[i];
+  
+  cout<<solve()<<endl;
+  
+  return 0;
+}
