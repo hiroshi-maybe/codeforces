ee1a07389c5d0cb5e692488565ceefe1bbe5a704
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-10-19 21:53:46 -0700

    solve Mail.Ru Cup 2018 Round 1 div1+div2 B. Appending Mex

---
+++ solutions/AppendingMex.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+#include <iomanip>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline bool SMIN(T &l,const T &r){return l<r?l=r,1:0;}
+template<class T> inline bool SMAX(T &l,const T &r){return l>r?l=r,1:0;}
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// debug cerr
+#define TRACE true
+#define dump(x) if(TRACE) { cerr << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+
+/*
+ 
+ 10/18/2018 Mail.Ru Cup 2018 R1
+ 
+ 9:45-9:59 1WA on test case #12
+ 10:01 AC
+ 
+ http://codeforces.com/blog/entry/62563
+ 
+ */
+
+// 9:59 1WA #12
+// 10:01 submit
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG x.cpp && ./a.out
+const int MAX_N=1e5+10;
+LL A[MAX_N];
+int N;
+
+LL solve() {
+  SETI S;
+  if(A[0]!=0) return 1;
+  S.emplace(A[0]);
+  FOR(i,1,N) {
+    int n=A[i];
+    if(n>0&&S.count(n-1)==0) return i+1;
+    S.emplace(n);
+  }
+  return -1;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  
+  cin>>N;
+  REP(i,N) cin>>A[i];
+  
+  cout<<solve()<<endl;
+  
+  return 0;
+}
