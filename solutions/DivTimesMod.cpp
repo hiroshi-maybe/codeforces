203e6fb1283902b70190d3eb9ddd88c55f8a427e
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-12-30 14:44:59 -0800

    solve R528 div2 B. Div Times Mod

---
+++ solutions/DivTimesMod.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+#include <iomanip>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<vector<int>> VVI;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+
+// debug cerr
+#define TRACE true
+#define dump(x) if(TRACE) { cerr << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpf(args...) if(TRACE) { fprintf(stderr, ##args); putchar('\n'); }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+template<class Iter> void dumpc(Iter begin, Iter end) { if (TRACE) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; } }
+
+// $ ../batch DivTimesMod | diff DivTimesMod.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address DivTimesMod.cpp && ./a.out
+
+/*
+ 
+ 12/23/2018
+ 
+ 05:43-6:10 AC
+ 
+ https://codeforces.com/blog/entry/64078
+ 
+ floor(a/b)=(a-a%b)/b
+ Thus x = N/p*k+p
+ 
+ */
+
+LL N,K;
+const LL Inf=1e10;
+LL f(LL a, LL b) {
+  LL res=Inf;
+  for(LL x=a*K; x<K*(1+a); ++x) {
+    if(x%K==b) SMIN(res,x);
+  }
+  return res;
+}
+
+void solve() {
+  LL res=Inf;
+  for(LL p=1; p*p<=N; ++p) if(N%p==0) {
+    SMIN(res,f(p,N/p));
+    if(p*p!=N) SMIN(res,f(N/p,p));
+  }
+//
+  if((res/K)*(res%K)!=N) {
+    dump3(N,K,res); dump3((res/K),(res%K),(res/K)*(res%K));
+  }
+  assert((res/K)*(res%K)==N);
+  cout<<res<<endl;
+}
+#include <chrono>
+#include <random>
+// mt19937_64 for 64 bit unsigned integer
+//mt19937 rnd(time(nullptr));
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+int genRandNum(int lb, int ub) {
+  // Helper to generate random integer in range [lb, ub)
+  int x = rnd() % (int)(ub - lb);
+  return x + lb;
+}
+void test() {
+  N=genRandNum(1,1e6+1),K=genRandNum(2,1001);
+  solve();
+}
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  
+//  while(true) test();
+  
+  cin>>N>>K;
+  solve();
+  
+  return 0;
+}
