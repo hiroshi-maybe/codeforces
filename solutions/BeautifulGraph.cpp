3e86cadf24be311de471b2dc0328063e545503c7
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-01-06 21:32:09 -0800

    add solution of E56 div2 D. Beautiful Graph

---
+++ solutions/BeautifulGraph.cpp
@@ -0,0 +1,232 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+#include <iomanip>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<vector<int>> VVI;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+
+// debug cerr
+#define TRACE true
+#define dump(x) if(TRACE) { cerr << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpf(args...) if(TRACE) { fprintf(stderr, ##args); putchar('\n'); }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+template<class Iter> void dumpc(Iter begin, Iter end) { if (TRACE) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; } }
+
+// $ ../batch BeautifulGraph | diff BeautifulGraph.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address BeautifulGraph.cpp && ./a.out
+
+/*
+ 
+ 1/6/2019
+ 
+ 17:50-18:30 WA. give up.
+ 20:20-21:10 fixed bug referring to failed test case.
+ Array initialization was causing TLE. Very stupid problem 🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕🖕
+ 
+ */
+
+const int MOD=998244353;
+struct ModInt {
+  unsigned int val;
+  ModInt(): val(0) {}
+  ModInt(int v) { norm(v%MOD); }
+  ModInt(long long v) { norm(v%MOD); }
+  ModInt& norm(long long v) {
+    v=v<0?v%MOD+MOD:v; // negative
+    v=v>=MOD?v-MOD:v; // mod
+    val=(unsigned int)v;
+    return *this;
+  }
+  explicit operator bool() const { return val!=0; }
+  ModInt operator-() const { return ModInt(0)-*this; }
+  ModInt &operator+=(ModInt that) { return norm((long long)val+that.val); }
+  ModInt &operator-=(ModInt that) { return norm((long long)val-that.val); }
+  ModInt &operator*=(ModInt that) { val=(unsigned long long)val*that.val%MOD; return *this; }
+  ModInt &operator/=(ModInt that) { return *this*=that.inv(); }
+  ModInt operator+(ModInt that) const { return ModInt(*this)+=that; }
+  ModInt operator-(ModInt that) const { return ModInt(*this)-=that; }
+  ModInt operator*(ModInt that) const { return ModInt(*this)*=that; }
+  ModInt operator/(ModInt that) const { return ModInt(*this)/=that; }
+  ModInt pow(long long n) const {
+    ModInt x=*this, res=1;
+    while(n>0) {
+      if(n&1) res*=x;
+      x*=x,n>>=1;
+    }
+    return res;
+  }
+  ModInt inv() const { return (*this).pow(MOD-2); }
+  bool operator==(ModInt that) const { return val==that.val; }
+  bool operator!=(ModInt that) const { return val!=that.val; }
+  friend ostream& operator<<(ostream& os, const ModInt& that) { return os<<that.val; }
+};
+struct UF {
+public:
+  int groupNum;
+  UF(int N) {
+    uf=vector<int>(N);
+    rank=vector<int>(N);
+    for(int i=0; i<N; ++i) uf[i]=i,rank[i]=0;
+    groupNum=N;
+  }
+  
+  int find(int i) {
+    return uf[i]==i?i:uf[i]=find(uf[i]);
+  }
+  
+  bool same(int x, int y) {
+    return find(x)==find(y);
+  }
+  
+  void unite(int x, int y) {
+    x=find(x);
+    y=find(y);
+    if(x==y) return;
+    groupNum--;
+    
+    if (rank[x]==rank[y]) {
+      uf[x]=y;
+      rank[y]++;
+      return;
+    }
+    
+    if (rank[x]<rank[y]) {
+      uf[x]=y;
+    } else {
+      uf[y] =x;
+      if(rank[x]==rank[y]) ++rank[x];
+    }
+  }
+private:
+  vector<int> rank;
+  vector<int> uf;
+};
+
+int N,M;
+const int MAX_N=1e6;
+int T;
+
+// const int Inf=1e8;
+//int viz[MAX_N];
+VI viz;
+int cnt[2];
+bool dfs(int u, int par,VV<int> &G) {
+  if(viz[u]>=0) {
+    return viz[u]==par;
+  }
+  viz[u]=par;
+  cnt[par]++;
+  FORR(v,G[u]) {
+    if(!dfs(v,par^1,G)) return false;
+  }
+  // counting # of `0` fails if there is only one vertex in the connected component 🖕
+//  res.first+=par^1,res.second+=1;
+//  return res;
+  return true;
+}
+ModInt pow2[MAX_N];
+void solve(/*UF &uf,*/VV<int> &G) {
+  ModInt res(1);
+  viz=VI(N,-1);
+//  MINUS(viz);
+  REP(i,N) if(viz[i]==-1) {
+//    ZERO(cnt);
+    cnt[0]=cnt[1]=0;
+//    II p=dfs(i,0,G);
+    if(!dfs(i,0,G)) {
+      res=ModInt(0);
+      break;
+    }
+      /*
+    if(p.first<=0) {
+      res=ModInt(0);
+      break;
+    }*/
+//    res*=(ModInt(2).pow(p.first)+ModInt(2).pow(p.second-p.first));
+//    res*=pow2[p.first]+pow2[p.second-p.first];
+    res*=pow2[cnt[0]]+pow2[cnt[1]];
+  }
+  println("%d",res.val);
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  
+  /*
+  REP(i,100) {
+    println("%d %d",i+1,(i+1)%100+1);
+  }*/
+  
+  pow2[0]=ModInt(1);
+  REP(i,MAX_N-1) pow2[i+1]=pow2[i]*ModInt(2);
+  
+  scanf("%d",&T);
+  REP(i,T) {
+    scanf("%d %d",&N,&M);
+    VV<int> G(N);
+    //UF uf(N);
+    
+    REP(_,M) {
+      int u,v; scanf("%d %d",&u,&v);
+      --u,--v;
+      G[u].push_back(v);
+      G[v].push_back(u);
+      //uf.unite(u,v);
+    }
+//    dump(uf.groupNum);
+    solve(/*uf,*/G);
+  }
+  
+  return 0;
+}
