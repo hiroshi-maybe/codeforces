dfcb113f31a9fa989ed467277e934589ae1d40cf
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-01-07 15:01:39 -0800

    solve AvitoCC div1+2 B. Farewell Party

---
+++ solutions/FarewellParty.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+#include <iomanip>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<vector<int>> VVI;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+
+// debug cerr
+#define TRACE true
+#define dump(x) if(TRACE) { cerr << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpf(args...) if(TRACE) { fprintf(stderr, ##args); putchar('\n'); }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+template<class Iter> void dumpc(Iter begin, Iter end) { if (TRACE) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; } }
+
+// $ ../batch FarewellParty | diff FarewellParty.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address FarewellParty.cpp && ./a.out
+
+/*
+ 
+ 1/7/2019
+ 
+ 10:12-10:37 AC
+ 
+ https://codeforces.com/blog/entry/63888
+ https://betrue12.hateblo.jp/entry/2018/12/17/213944
+ 
+ */
+
+const int MAX_N=1e6+1;
+LL A[MAX_N];
+int N;
+
+void nope() {
+  println("Impossible");
+  exit(0);
+}
+void solve() {
+  map<int,VI> M;
+  REP(i,N) {
+    A[i]=N-A[i];
+    M[A[i]].push_back(i);
+  }
+  int t=0;
+  VI res(N,0);
+  FORR(kvp,M) {
+    int cnt=kvp.first;
+    VI ids=kvp.second;
+    if(SZ(ids)%cnt!=0) nope();
+    REP(i,SZ(ids)) {
+      int j=ids[i];
+      if(i%cnt==0) ++t;
+      res[j]=t;
+    }
+  }
+  assert(count(ALL(res),0)==0);
+  println("Possible");
+  REP(i,N) printf("%d ",res[i]);
+  println("");
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  
+  cin>>N;
+  REP(i,N) cin>>A[i];
+  solve();
+  
+  return 0;
+}
