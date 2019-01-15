b506c8e4f2dd8091960cfe6022bae37611b92e5d
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-01-15 13:58:10 -0800

    solve R532 div2 A. Roman and Browser

---
+++ solutions/RomanAndBrowser.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+#include <iomanip>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<vector<int>> VVI;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+
+// debug cerr
+#define TRACE true
+#define dump(x) if(TRACE) { cerr << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpf(args...) if(TRACE) { fprintf(stderr, ##args); putchar('\n'); }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+template<class Iter> void dumpc(Iter begin, Iter end) { if (TRACE) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; } }
+
+// $ ../batch RomanAndBrowser | diff RomanAndBrowser.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address RomanAndBrowser.cpp && ./a.out
+
+/*
+ 
+ 1/13/2019
+ 
+ 6:35-6:42 AC
+ 
+ https://codeforces.com/blog/entry/64545
+ https://codeforces.com/blog/entry/64543
+ 
+ */
+
+const int MAX_N=1e6+1;
+LL A[MAX_N];
+int N,K;
+
+void solve() {
+  int res=0;
+  
+  int E=0,S=0;
+  REP(i,N) (A[i]==-1?S:E)++;
+  
+  REP(b,N) {
+    int e=E,s=S;
+    REP(c,N) if(abs(c-b)%K==0) {
+      if(A[c]==-1) --s;
+      else --e;
+    }
+    SMAX(res,abs(e-s));
+  }
+  cout<<res<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  
+  cin>>N>>K;
+  REP(i,N) cin>>A[i];
+  solve();
+  
+  return 0;
+}
