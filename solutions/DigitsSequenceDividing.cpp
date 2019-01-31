59c979328df8df367d3ca8304b6339f9c1c5ef79
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-01-30 21:07:14 -0800

    solve ER59 div2 A. Digits Sequence Dividing

---
+++ solutions/DigitsSequenceDividing.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+#include <iomanip>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<vector<int>> VVI;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+
+// debug cerr
+#define TRACE true
+#define dump(x) if(TRACE) { cerr << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpf(args...) if(TRACE) { fprintf(stderr, ##args); putchar('\n'); }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+template<class Iter> void dumpc(Iter begin, Iter end) { if (TRACE) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; } }
+
+// $ cp-batch DigitsSequenceDividing | diff DigitsSequenceDividing.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address DigitsSequenceDividing.cpp && ./a.out
+
+/*
+ 
+ 1/26/2019
+ 
+ 7:35-7:46 AC
+ 
+ https://codeforces.com/blog/entry/64833
+ https://codeforces.com/blog/entry/64847
+ 
+ */
+
+int Q,N;
+string S;
+void nope() {
+  println("NO");
+}
+void solve() {
+  if(N==2) {
+    if(S[0]>=S[1])nope();
+    else { println("YES"); println("2"); println("%c %c",S[0],S[1]); }
+    return;
+  }
+  println("YES"); println("2");
+  println("%s %s",S.substr(0,1).c_str(),S.substr(1,SZ(S)-1).c_str());
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  
+  cin>>Q;
+  REP(i,Q) {
+    cin>>N>>S;
+    solve();
+  }
+  
+  return 0;
+}
