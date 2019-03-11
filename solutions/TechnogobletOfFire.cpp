70f1890fb8c7a72ac789a9e3e5a42c6964637796
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-03-11 17:23:10 +0900

    solve R543 div2 A. Technogoblet of Fire

---
+++ solutions/TechnogobletOfFire.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+//#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch TechnogobletOfFire | diff TechnogobletOfFire.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address TechnogobletOfFire.cpp && ./a.out
+
+/*
+ 
+ 3/3/2019
+ 
+ 7:35-7:50 AC
+ 
+ https://codeforces.com/blog/entry/65679
+ 
+ */
+
+const int MAX_N=1e6+1;
+int P[MAX_N],S[MAX_N],C[MAX_N];
+int N,M,K;
+
+bool isstrongest(int x) {
+  int p0=P[x];
+  dump(x,P[x],S[x],N);
+  REP(i,N) {
+    dump(i,i!=x,S[i]==S[x],P[i]>p0);
+    if(i!=x&&S[i]==S[x]&&P[i]>p0) return false;
+  }
+  return true;
+}
+void solve() {
+  int res=0;
+  REP(i,K) res+=!isstrongest(C[i]-1);
+  cout<<res<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  
+  cin>>N>>M>>K;
+  REP(i,N) cin>>P[i];
+  REP(i,N) cin>>S[i];
+  REP(i,K) cin>>C[i];
+  solve();
+  
+  return 0;
+}
