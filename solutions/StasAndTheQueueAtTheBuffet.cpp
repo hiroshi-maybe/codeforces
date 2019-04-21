db4ba185417243184317672f41d7a952ab90bef9
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-04-21 15:54:47 -0700

    solve R553 div2 D. Stas and the Queue at the Buffet

---
+++ solutions/StasAndTheQueueAtTheBuffet.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch StasAndTheQueueAtTheBuffet | diff StasAndTheQueueAtTheBuffet.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address StasAndTheQueueAtTheBuffet.cpp && ./a.out
+
+/*
+ 
+ 4/18/2019
+ 
+ 9:33-9:51 AC
+ 
+ https://codeforces.com/blog/entry/66602
+ https://naoyat.hatenablog.jp/entry/CodeforcesRound553Div2
+ https://cinnamo-coder.hatenablog.com/entry/2019/04/19/123713
+ 
+ */
+
+const int MAX_N=1e6+1;
+LL A[MAX_N],B[MAX_N];
+int N;
+
+void solve() {
+  vector<pair<LL,int>> X(N);
+  REP(i,N) X[i]={A[i]-B[i],i};
+  sort(ALL(X)),reverse(ALL(X));
+  LL res=0;
+  REP(i,N) res+=B[i]*N-A[i];
+//  dump(res);
+  REP(j,N) {
+    LL a; int i; tie(a,i)=X[j];
+    res+=a*(j+1);
+  }
+  cout<<res<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  
+  cin>>N;
+  REP(i,N) cin>>A[i]>>B[i];
+  solve();
+  
+  return 0;
+}
