a17594202ae653117a1eef2cd0249867a9fc5693
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-05-14 10:43:08 -0700

    solve R549 div2 C. Queen

---
+++ solutions/Queen.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch Queen | diff Queen.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address Queen.cpp && ./a.out
+
+/*
+ 
+ 5/13/2019
+ 
+ 20:12-20:35 AC
+ 
+ https://cinnamo-coder.hatenablog.com/entry/2019/03/30/000000
+ https://codeforces.com/blog/entry/66301
+ 
+ */
+
+const int MAX_N=1e6+1;
+VI G[MAX_N];
+int N,C[MAX_N],root=-1;
+
+VI res;
+void dfs(int u, int pre) {
+  bool ok=C[u];
+  FORR(v,G[u]) if(v!=pre) {
+    if(C[v]==0) ok=false;
+    dfs(v,u);
+  }
+//  dump(u,ok);
+//  dumpc(G[u]);
+  if(ok)res.push_back(u);
+}
+
+void solve() {
+  dfs(root,-1);
+  sort(ALL(res));
+  if(res.empty()) res.push_back(-1);
+  FORR(x,res) printf("%d ", x);
+  println("");
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  
+  cin>>N;
+  REP(i,N) {
+    int u=i+1;
+    int v,c; cin>>v>>c;
+    if(v==-1) root=u;
+    else G[v].push_back(u);
+    C[u]=c;
+  }
+  solve();
+  
+  return 0;
+}
