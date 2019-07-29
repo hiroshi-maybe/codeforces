9e9fb53dfa55a826fdfc5743dec2320545a75b9c
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-07-29 12:19:49 -0700

    solve R557 div2 B. Double Matrix

---
+++ solutions/DoubleMatrix.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch DoubleMatrix | diff DoubleMatrix.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address DoubleMatrix.cpp && ./a.out
+
+/*
+ 
+ 7/26/2019
+ 
+ 12:53-13:10 AC
+ 
+ https://codeforces.com/blog/entry/66878
+ 
+ */
+
+const int MAX_N=1e2+1;
+int A[MAX_N][MAX_N],B[MAX_N][MAX_N];
+int N,M;
+
+int C[2][MAX_N][MAX_N];
+void nope() {
+  println("Impossible");
+  exit(0);
+}
+void solve() {
+  REP(i,N)REP(j,M) {
+    C[0][i][j]=max(A[i][j],B[i][j]);
+    C[1][i][j]=min(A[i][j],B[i][j]);
+  }
+  REP(i,N)REP(j,M)REP(k,2) {
+    if(i<N-1&&C[k][i][j]>=C[k][i+1][j]) nope();
+    if(j<M-1&&C[k][i][j]>=C[k][i][j+1]) nope();
+  }
+  println("Possible");
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  
+  cin>>N>>M;
+  REP(i,N)REP(j,M) cin>>A[i][j];
+  REP(i,N)REP(j,M) cin>>B[i][j];
+  solve();
+  
+  return 0;
+}
