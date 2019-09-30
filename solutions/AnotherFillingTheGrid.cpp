298b54c8c21379ea4723046e1a2d01042a0c74ca
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-09-29 21:51:41 -0700

    add solution of R589 div2 E. Another Filling the Grid

---
+++ solutions/AnotherFillingTheGrid.cpp
@@ -0,0 +1,163 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+const int MOD=1000000007;
+//const int MOD=998244353;
+struct ModInt {
+  unsigned int val;
+  ModInt(): val(0) {}
+  ModInt(int v) { norm(v%MOD); }
+  ModInt(long long v) { norm(v%MOD); }
+  ModInt& norm(long long v) {
+    v=v<0?v%MOD+MOD:v; // negative
+    v=v>=MOD?v-MOD:v; // mod
+    val=(unsigned int)v;
+    return *this;
+  }
+  explicit operator bool() const { return val!=0; }
+  ModInt operator-() const { return ModInt(0)-*this; }
+  ModInt &operator+=(ModInt that) { return norm((long long)val+that.val); }
+  ModInt &operator-=(ModInt that) { return norm((long long)val-that.val); }
+  ModInt &operator*=(ModInt that) { val=(unsigned long long)val*that.val%MOD; return *this; }
+  ModInt &operator/=(ModInt that) { return *this*=that.inv(); }
+  ModInt operator+(ModInt that) const { return ModInt(*this)+=that; }
+  ModInt operator-(ModInt that) const { return ModInt(*this)-=that; }
+  ModInt operator*(ModInt that) const { return ModInt(*this)*=that; }
+  ModInt operator/(ModInt that) const { return ModInt(*this)/=that; }
+  ModInt pow(long long n) const {
+    ModInt x=*this, res=1;
+    while(n>0) {
+      if(n&1) res*=x;
+      x*=x,n>>=1;
+    }
+    return res;
+  }
+  ModInt inv() const { return (*this).pow(MOD-2); }
+  bool operator==(ModInt that) const { return val==that.val; }
+  bool operator!=(ModInt that) const { return val!=that.val; }
+  friend ostream& operator<<(ostream& os, const ModInt& that) { return os<<that.val; }
+};
+LL choose(LL n, LL k) {
+  if(n<k) return 0;
+  
+  const int MAX_N = 1e6+1;
+  assert(0<=k&&k<=MAX_N);
+  static LL fact[MAX_N+1],revfact[MAX_N+1],rev[MAX_N+1];
+  
+  if(rev[1]==0) {
+    rev[1]=1;
+    for(int i=2;i<=MAX_N;i++) rev[i]=rev[MOD%i]*(MOD-MOD/i)%MOD;
+    fact[0]=1,revfact[0]=1;
+    for(int i=1;i<=MAX_N;i++) {
+      fact[i]=fact[i-1]*i%MOD;
+      revfact[i]=revfact[i-1]*rev[i]%MOD;
+    }
+  }
+  return fact[n]*revfact[n-k]%MOD*revfact[k]%MOD;
+}
+
+// $ cp-batch AnotherFillingTheGrid | diff AnotherFillingTheGrid.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address AnotherFillingTheGrid.cpp && ./a.out
+
+/*
+ 
+ 9/29/2019
+ 
+ 7:49-8:05 give up
+ 20:00-21:36 read editorial and got AC
+ 
+ https://codeforces.com/blog/entry/70162
+ https://twitter.com/hanseilimak/status/1178532232333365248
+ 
+ */
+
+const int MAX_N=251;
+int N,K;
+
+bool viz[MAX_N][MAX_N];
+ModInt memo[MAX_N][MAX_N];
+ModInt kpow[MAX_N],k1pow[MAX_N];
+
+ModInt f(int r, int c) {
+  if(viz[r][c]) return memo[r][c];
+  ModInt &res=memo[r][c];
+  viz[r][c]=1;
+  if(r==0) return res=(c==0);
+  if(c==0) {
+    return res=(kpow[N]-k1pow[N]).pow(r);
+  }
+  if(r==1) {
+    return res=kpow[N-c];
+  }
+  ModInt a;
+  {
+    a=(kpow[N-c]-k1pow[N-c])*k1pow[c]*f(r-1,c);
+  }
+  ModInt b=0;
+  {
+    ModInt x1=kpow[N-c];
+    FORE(x,1,c) {
+      ModInt x2=choose(c,x),x3=k1pow[c-x];
+      b+=x1*x2*x3*f(r-1,c-x);
+    }
+  }
+  return memo[r][c]=a+b;
+}
+void solve() {
+  ZERO(viz);
+  REPE(i,N) kpow[i]=ModInt(K).pow(i),k1pow[i]=ModInt(K-1).pow(i);
+  ModInt res=f(N,N);
+  cout<<res<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  
+  cin>>N>>K;
+  solve();
+  
+  return 0;
+}
