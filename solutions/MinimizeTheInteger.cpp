c2855c477db7ffa50dc16eb43b44d368bfce64a9
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-10-26 17:45:16 -0700

    solve E75 div2 C. Minimize The Integer

---
+++ solutions/MinimizeTheInteger.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch MinimizeTheInteger | diff MinimizeTheInteger.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address MinimizeTheInteger.cpp && ./a.out
+
+/*
+ 
+ 10/24/2019
+ 
+ 9:04-9:17 AC
+ 
+ https://codeforces.com/blog/entry/70860
+ 
+ */
+
+//const int MAX_N=1e6+1;
+string S;
+int N;
+
+void solve() {
+  N=SZ(S);
+  vector<queue<char>> X(2);
+  REP(i,N) {
+    int d=S[i]-'0';
+    X[d%2].push(S[i]);
+  }
+  string res;
+  while(SZ(X[0])||SZ(X[1])) {
+    if(X[0].empty()) res+=X[1].front(),X[1].pop();
+    else if(X[1].empty()) res+=X[0].front(),X[0].pop();
+    else if(X[0].front()<X[1].front()) {
+      res+=X[0].front(),X[0].pop();
+    } else {
+      res+=X[1].front(),X[1].pop();
+    }
+  }
+  cout<<res<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  
+  int T; cin>>T;
+  REP(i,T) {
+    cin>>S;
+    solve();
+  }
+  
+  return 0;
+}
