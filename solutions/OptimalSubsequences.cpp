82bef974ead7bd9d1e27a4e58b5b3066a0450335
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-11-24 16:46:57 -0800

    solve R602 div2 D. Optimal Subsequences

---
+++ solutions/OptimalSubsequences.cpp
@@ -0,0 +1,142 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+template <typename T> struct BIT {
+public:
+  int N;
+  vector<T> A;
+  BIT() {}
+  BIT(int N) { init(N); }
+  void init(int N) {
+    this->N=N;
+    A=vector<T>(N+1,0);
+  }
+  // query ∑ { sum[i] : i=l..<r }
+  T query(int l, int r) {
+    assert(l<=r&&0<=l&&r<=N);
+    return query(r)-query(l);
+  }
+  // min index s.t. dat[0..i]>x
+  int upperbound(T x) {
+    int good=N+1,bad=0;
+    while(abs(good-bad)>1) {
+      int m=(good+bad)/2;
+      (query(m)>x?good:bad)=m;
+    }
+    return good-1;
+  }
+  // sum[i]+=x
+  void add(int i, T x) {
+    assert(0<=i&&i<N);
+    ++i;
+    while(i<=N) A[i]+=x,i+=lsone(i);
+  }
+  // sum[i]=x
+  void update(int i, T v) {
+    T pre=query(i+1)-query(i);
+    add(i,v-pre);
+  }
+private:
+  int lsone(int i) { return i&-i; }
+  // query in [0,r) : ∑ { sum[i] : i=0..r-1 }
+  T query(int r) {
+    assert(0<=r&&r<=N);
+    T res=0;
+    while(r>0) res+=A[r],r-=lsone(r);
+    return res;
+  }
+};
+
+// $ cp-batch OptimalSubsequences | diff OptimalSubsequences.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address OptimalSubsequences.cpp && ./a.out
+
+/*
+ 
+ 11/24/2019
+ 
+ 1:00-1:29 AC
+ 
+ https://twitter.com/hanseilimak/status/1198764813314220032
+ 
+ */
+
+const int MAX_N=1e6+1;
+int A[MAX_N];
+int N;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  
+  cin>>N;
+  REP(i,N) cin>>A[i];
+  
+  vector<II> X;
+  REP(i,N)X.emplace_back(-A[i],i);
+  sort(ALL(X));
+  
+  int M;
+  cin>>M;
+  vector<III> Q(M);
+  REP(i,M) {
+    int K,P; cin>>K>>P;
+    Q.emplace_back(K,P,i);
+  }
+  sort(ALL(Q));
+  BIT<int> bit(N+10);
+  int i=0;
+  VI res(M,-1);
+  FORR(tup,Q) {
+    int K,rank,qi; tie(K,rank,qi)=tup;
+    while(i<K) {
+      int j; tie(ignore,j)=X[i++];
+      bit.add(j,1);
+    }
+    int ans=bit.upperbound(rank-1);
+    res[qi]=ans;
+  }
+  REP(i,M) println("%d",A[res[i]]);
+  
+  return 0;
+}
