f201c6d8afa7a35238e04c31cfa87de766c7cab9
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-11-30 12:36:55 -0800

    solve R603 div2 C. Everyone is a Winner!

---
+++ solutions/EveryoneIsAWinner.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch EveryoneIsAWinner | diff EveryoneIsAWinner.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address EveryoneIsAWinner.cpp && ./a.out
+
+/*
+ 
+ 11/29/2019
+ 
+ 7:10-7:42 AC
+ 
+ https://codeforces.com/blog/entry/71844
+ 
+ */
+#define UNIQUE(A) sort((A).begin(),(A).end()),(A).erase(std::unique((A).begin(), (A).end()), (A).end());
+LL N;
+
+void solve() {
+  vector<LL> res;
+  LL last=-1;
+  for(LL p=1; p*p<=N; ++p) {
+    res.push_back(N/p);
+    last=p;
+  }
+  assert(last>=1);
+  for(LL x=N/(last+1); x>=0; --x) res.push_back(x);
+  UNIQUE(res);
+  println("%d",SZ(res));
+  REP(i,SZ(res)) printf("%lld ",res[i]);
+  println("");
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  
+  /*
+  FOR(n,1,100) {
+    set<int> S;
+    FORE(k,1,n+1) {
+      S.emplace(n/k);
+    }
+    dump(n,SZ(S));
+    dumpc(S);
+  }
+  
+  return 0;*/
+  
+  int T; cin>>T;
+  REP(i,T) {
+    cin>>N;
+    solve();
+  }
+
+  return 0;
+}
