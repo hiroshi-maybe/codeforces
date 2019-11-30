7030e961880aeeeafacc8b47349f87b3553ab7a2
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-11-30 12:39:07 -0800

    solve R603 div2 D. Secret Passwords

---
+++ solutions/SecretPasswords.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+struct UF {
+public:
+  int N,G/* # of disjoint sets */;
+  UF(int N): N(N) { init(N); }
+  void init(int N) {
+    par=vector<int>(N),sz=vector<int>(N,1);
+    for(int i=0; i<N; ++i) par[i]=i,sz[i]=1;
+    G=N;
+  }
+  int find(int x) { return par[x]==x?x:par[x]=find(par[x]); }
+  int size(int x) { return sz[find(x)]; }
+  bool sameset(int x, int y) { return find(x)==find(y); }
+  int unite(int x, int y) {
+    x=find(x),y=find(y);
+    if(x==y) return x;
+    int p=x,c=y;
+    if(sz[p]<sz[c]) swap(p,c);
+    G--,sz[p]=sz[c]=sz[p]+sz[c];
+    par[c]=p;
+    return p;
+  }
+private:
+  vector<int> par,sz;
+};
+// $ cp-batch SecretPasswords | diff SecretPasswords.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address SecretPasswords.cpp && ./a.out
+
+/*
+ 
+ 11/29/2019
+ 
+ 7:42-7:54 AC
+ 
+ https://codeforces.com/blog/entry/71844
+ 
+ */
+
+const int MAX_N=1e6+1;
+string S[MAX_N];
+int N;
+
+void solve() {
+  UF uf(26);
+  VI used(26,0);
+  REP(i,N) {
+    int L=SZ(S[i]);
+    string s=S[i];
+    REP(j,L-1) uf.unite(s[j]-'a',s[j+1]-'a'),used[s[j]-'a']=1;
+    used[s[L-1]-'a']=1;
+  }
+  int res=uf.G-count(ALL(used),0);
+  cout<<res<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  
+  cin>>N;
+  REP(i,N) cin>>S[i];
+  solve();
+  
+  return 0;
+}
