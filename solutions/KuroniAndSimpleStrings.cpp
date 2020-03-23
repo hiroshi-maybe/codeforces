0f8b473802a63f4644261023a6f62c92c03b8b3f
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-03-22 22:33:47 -0700

    solve Ozon2020 div1+2 B. Kuroni and Simple Strings

---
+++ solutions/KuroniAndSimpleStrings.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch KuroniAndSimpleStrings | diff KuroniAndSimpleStrings.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address KuroniAndSimpleStrings.cpp && ./a.out
+
+/*
+ 
+ 3/22/2020
+ 
+ 16:10-16:52 AC
+ 
+ https://codeforces.com/blog/entry/74459
+ 
+ */
+
+string S;
+int N;
+
+void solve() {
+  N=SZ(S);
+  VI res;
+  int r=N-1;
+  REP(l,N) if(S[l]=='(') {
+    while(l<r&&S[r]=='(') --r;
+    if(l>=r) break;
+    assert(S[r]==')');
+//    dump(l,r,S[l],S[r]);
+    res.push_back(l),res.push_back(r);
+    --r;
+  }
+
+  sort(ALL(res));
+  if(SZ(res)==0) {
+    cout<<0<<endl;
+  } else {
+    cout<<1<<endl;
+    cout<<SZ(res)<<endl;
+    REP(i,SZ(res)) cout<<res[i]+1<<" \n"[i==SZ(res)-1];
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  
+  cin>>S;
+  solve();
+  
+  return 0;
+}
