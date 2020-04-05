68a059dec8e28694d908254615014f92104f5386
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-04-05 15:20:34 -0700

    add solution of R631 div2 D. Dreamoon Likes Sequences

---
+++ solutions/DreamoonLikesSequences.cpp
@@ -0,0 +1,128 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+int MOD=1000000007;
+//const int MOD=998244353;
+struct ModInt {
+  unsigned int val;
+  ModInt(): val(0) {}
+  ModInt(int v) { norm(v%MOD); }
+  ModInt(long long v) { norm(v%MOD); }
+  ModInt& norm(long long v) {
+    v=v<0?v%MOD+MOD:v; // negative
+    v=v>=MOD?v-MOD:v; // mod
+    val=(unsigned int)v;
+    return *this;
+  }
+  explicit operator bool() const { return val!=0; }
+  ModInt operator-() const { return ModInt(0)-*this; }
+  ModInt &operator+=(ModInt that) { return norm((long long)val+that.val); }
+  ModInt &operator-=(ModInt that) { return norm((long long)val-that.val); }
+  ModInt &operator*=(ModInt that) { val=(unsigned long long)val*that.val%MOD; return *this; }
+  ModInt &operator/=(ModInt that) { return *this*=that.inv(); }
+  ModInt operator+(ModInt that) const { return ModInt(*this)+=that; }
+  ModInt operator-(ModInt that) const { return ModInt(*this)-=that; }
+  ModInt operator*(ModInt that) const { return ModInt(*this)*=that; }
+  ModInt operator/(ModInt that) const { return ModInt(*this)/=that; }
+  ModInt pow(long long n) const {
+    ModInt x=*this, res=1;
+    while(n>0) {
+      if(n&1) res*=x;
+      x*=x,n>>=1;
+    }
+    return res;
+  }
+  ModInt inv() const { return (*this).pow(MOD-2); }
+  bool operator==(ModInt that) const { return val==that.val; }
+  bool operator!=(ModInt that) const { return val!=that.val; }
+  friend ostream& operator<<(ostream& os, const ModInt& that) { return os<<that.val; }
+};
+// $ cp-batch DreamoonLikesSequences | diff DreamoonLikesSequences.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address DreamoonLikesSequences.cpp && ./a.out
+
+/*
+ 
+ 4/3/2020
+ 
+ 9:19-9:35 give up
+ 
+ 4/5/2020
+ 
+ 14:25-15:16 read editorials and got AC
+ 
+ https://twitter.com/hanseilimak/status/1246925612037464064
+ https://codeforces.com/blog/entry/75559
+ https://twitter.com/laycrs/status/1246114128084656131
+ https://twitter.com/neterukun_cd/status/1246116330895323136
+ https://twitter.com/ngtkana/status/1246115957992378369
+ https://twitter.com/kzyKT_M/status/1246115216913387520
+ https://twitter.com/tomerun/status/1246115776265740288
+ https://twitter.com/ei1333/status/1246114600690388992
+ 
+ */
+
+int D,M;
+
+void solve() {
+  MOD=M;
+  ModInt res=1;
+  REP(i,31) {
+    if(D<(1<<i)) break;
+    res*=min(D,(1<<(i+1))-1)-(1<<i)+2;
+  }
+  cout<<res-1<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  
+  int T; cin>>T;
+  REP(_,T) {
+    cin>>D>>M;
+    solve();
+  }
+  
+  return 0;
+}
