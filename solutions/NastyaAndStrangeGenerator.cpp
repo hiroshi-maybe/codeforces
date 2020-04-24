fafb27322bc892841646a10104c402a34f724356
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-04-23 22:02:17 -0700

    solve R637 div2 C. Nastya and Strange Generator

---
+++ solutions/NastyaAndStrangeGenerator.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+//#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch NastyaAndStrangeGenerator | diff NastyaAndStrangeGenerator.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address NastyaAndStrangeGenerator.cpp && ./a.out
+
+/*
+ 
+ 4/23/2020
+ 
+ 8:08-8:44 AC
+ 
+ */
+
+const int MAX_N=1e6+1;
+int A[MAX_N];
+int N;
+
+bool solve() {
+  set<II> Q;
+  VI ps(N);
+  REP(i,N) ps[A[i]-1]=i;
+  dumpc(ps);
+  VI cnt(N+1,1);
+  cnt[N]=-1;
+  REP(i,N)Q.emplace(1,i);
+  REP(i,N) {
+    dumpc(Q);
+    int p=ps[i];
+    auto it=Q.rbegin();
+    if(it==Q.rend()) return false;
+    int maxcnt=it->first;
+    dump(p,maxcnt);
+    dumpc(cnt);
+    if(cnt[p]!=maxcnt) return false;
+    assert(Q.count({maxcnt,p}));
+    Q.erase({maxcnt,p});
+    auto itit=Q.find({cnt[p+1],p+1});
+    if(itit!=Q.end()) {
+      Q.erase(itit);
+      cnt[p+1]+=cnt[p];
+      Q.emplace(cnt[p+1],p+1);
+    }
+    cnt[p]=-1;
+  }
+  return true;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+  
+  int T; cin>>T;
+  while(T--) {
+    cin>>N;
+    REP(i,N) cin>>A[i];
+    cout<<(solve()?"Yes":"No")<<endl;
+  }
+  
+  return 0;
+}
