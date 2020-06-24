9b1dfb592c5536b35eb8cc009c6ee6157abaa334
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-06-23 22:07:41 -0700

    solve R652 div2 B. AccurateLee

---
+++ solutions/AccurateLee.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch AccurateLee | diff AccurateLee.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address AccurateLee.cpp && ./a.out
+
+/*
+
+ 6/23/2020
+
+ 7:20-7:34 AC
+
+ https://codeforces.com/blog/entry/79235
+
+ */
+
+//const int MAX_N=1e6+1;
+string S;
+int N;
+
+void solve() {
+  string pre="",suf="";
+  REP(i,N) {
+    if(S[i]=='0') pre.push_back(S[i]);
+    else break;
+  }
+  string s=S.substr(SZ(pre));
+  for(int i=SZ(s)-1; i>=0; --i) {
+    if(s[i]=='1') suf.push_back(s[i]);
+    else break;
+  }
+  reverse(ALL(suf));
+  string ss=s.substr(0,SZ(s)-SZ(suf));
+  //dump(pre,ss,suf);
+  string res;
+  if(SZ(ss)) {
+    assert(count(ALL(ss),'1')>0);
+    assert(count(ALL(ss),'0')>0);
+    res=pre+"0"+suf;
+  } else {
+    res=pre+suf;
+  }
+  cout<<res<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+
+  int t; cin>>t;
+  while(t--) {
+    cin>>N>>S;
+    solve();
+  }
+
+  return 0;
+}
