45b582a2312678b4aebf579faf8d4e74bdf1010c
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-08-13 08:55:53 -0700

    solve R664 div2 C. Boboniu and Bit Operations

---
+++ solutions/BoboniuAndBitOperations.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch BoboniuAndBitOperations | diff BoboniuAndBitOperations.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address BoboniuAndBitOperations.cpp && ./a.out
+
+/*
+
+ 8/12/2020
+
+ 8:18-8:39 AC
+
+ https://twitter.com/hanseilimak/status/1293939118859120641
+ https://codeforces.com/blog/entry/81355
+
+ */
+
+const int MAX_N=200+1;
+int A[MAX_N],B[MAX_N];
+int N,M;
+
+//const int Inf=1<<9;
+bool submask(int mask1, int mask2) {
+  return (mask1|mask2)==mask1;
+}
+bool ok(int mask) {
+  REP(i,N) {
+    bool yes=false;
+    REP(j,M) if(submask(mask,A[i]&B[j])) yes=true;
+    //dump(i,A[i],yes);
+    if(!yes) return false;
+  }
+  return true;
+}
+int solve() {
+  REP(mask,1<<9) {
+    if(ok(mask)) return mask;
+  }
+  assert(false);
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+
+  cin>>N>>M;
+  REP(i,N) cin>>A[i];
+  REP(i,M) cin>>B[i];
+  cout<<solve()<<endl;
+
+  return 0;
+}
