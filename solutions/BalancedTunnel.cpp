84a67103732a9a9492a4dc5a22b8033b42d0b703
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-08-17 21:42:47 -0700

    solve GR5 div1+2 B. Balanced Tunnel

---
+++ solutions/BalancedTunnel.cpp
@@ -0,0 +1,145 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+template <typename T> struct BIT {
+public:
+  int N;
+  vector<T> A;
+  BIT() {}
+  BIT(int N) { init(N); }
+  void init(int N) {
+    this->N=N;
+    A=vector<T>(N+1,0);
+  }
+  // query ∑ { sum[i] : i=l..<r }
+  T query(int l, int r) {
+    assert(l<=r&&0<=l&&r<=N);
+    return query(r)-query(l);
+  }
+  // min index s.t. dat[0..i]>x
+  int upperbound(T x) {
+    int good=N+1,bad=0;
+    while(abs(good-bad)>1) {
+      int m=(good+bad)/2;
+      (query(m)>x?good:bad)=m;
+    }
+    return good-1;
+  }
+  // sum[i]+=x
+  void add(int i, T x) {
+    assert(0<=i&&i<N);
+    ++i;
+    while(i<=N) A[i]+=x,i+=lsone(i);
+  }
+  // sum[i]=x
+  void update(int i, T v) {
+    T pre=query(i+1)-query(i);
+    add(i,v-pre);
+  }
+private:
+  int lsone(int i) { return i&-i; }
+  // query in [0,r) : ∑ { sum[i] : i=0..r-1 }
+  T query(int r) {
+    assert(0<=r&&r<=N);
+    T res=0;
+    while(r>0) res+=A[r],r-=lsone(r);
+    return res;
+  }
+};
+
+// compute inversions
+vector<int> inversions(vector<int> ns) {
+  int maxv=*max_element(ns.begin(),ns.end());
+  BIT<int> bit(maxv+1);
+  int N=ns.size();
+  vector<int> res(N);
+  for(int i=N-1; i>=0; --i) {
+    // Count elements which stays in right side and smaller
+    res[i]=bit.query(0,ns[i]);
+    bit.add(ns[i],1);
+  }
+  return res;
+}
+// $ cp-batch BalancedTunnel | diff BalancedTunnel.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address BalancedTunnel.cpp && ./a.out
+
+/*
+
+ 8/15/2020
+
+ 16:19-16:33 AC
+
+ https://codeforces.com/blog/entry/70620
+
+ */
+
+const int MAX_N=1e6+1;
+int A[MAX_N],B[MAX_N];
+int N;
+
+void solve() {
+  VI ps(N),ps2(N);
+  REP(i,N) ps[A[i]]=i;
+  REP(i,N) ps2[i]=ps[B[i]];
+  int res=0;
+  BIT<int> bit(N);
+  for(int i=N-1; i>=0; --i) {
+    int cnt=bit.query(0,ps2[i]);
+    bit.add(ps2[i],1);
+    res+=cnt>0;
+  }
+  cout<<res<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+
+  cin>>N;
+  REP(i,N) cin>>A[i],--A[i];
+  REP(i,N) cin>>B[i],--B[i];
+  solve();
+
+  return 0;
+}
