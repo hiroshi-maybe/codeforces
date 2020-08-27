6db95c39a74658519b9b1614ca76960800c27e28
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-08-26 23:25:06 -0700

    solve E94 div2 C. Binary String Reconstruction

---
+++ solutions/BinaryStringReconstruction.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch BinaryStringReconstruction | diff BinaryStringReconstruction.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address BinaryStringReconstruction.cpp && ./a.out
+
+/*
+
+ 8/25/2020
+
+ 8:03-8:35 AC
+
+ https://twitter.com/hanseilimak/status/1298868912679919617
+ https://codeforces.com/blog/entry/81942
+ https://codeforces.com/blog/entry/81916
+
+ */
+
+string S;
+int X;
+
+void nope() {
+  cout<<-1<<endl;
+}
+void solve() {
+  int N=SZ(S);
+  string res(N,'?');
+  REP(i,N) if(S[i]=='0') {
+    if(i-X>=0&&res[i-X]=='1') {
+      nope();
+      return;
+    }
+    if(i+X<N&&res[i+X]=='1') {
+      nope();
+      return;
+    }
+    if(i-X>=0) res[i-X]='0';
+    if(i+X<N) res[i+X]='0';
+  }
+  REP(i,N) if(res[i]=='?') res[i]='1';
+  REP(i,N) if(S[i]=='1') {
+    if((i-X>=0&&res[i-X]=='1')||(i+X<N&&res[i+X]=='1')) continue;
+    nope();
+    return;
+  }
+  cout<<res<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+
+  int t; cin>>t;
+  while(t--) {
+    cin>>S>>X;
+    solve();
+  }
+
+  return 0;
+}
