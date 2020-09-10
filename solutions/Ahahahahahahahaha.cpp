0098c1639e9b22e2ea4ad27f4fc7142040a96137
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-09-09 23:00:55 -0700

    solve R669 div2 A. Ahahahahahahahaha

---
+++ solutions/Ahahahahahahahaha.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch Ahahahahahahahaha | diff Ahahahahahahahaha.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address Ahahahahahahahaha.cpp && ./a.out
+
+/*
+
+ 9/8/2020
+
+ 7:35-8:09 AC
+
+ https://twitter.com/hanseilimak/status/1303936328489291777
+ https://codeforces.com/blog/entry/82417
+
+ */
+
+const int MAX_N=1e6+1;
+int A[MAX_N];
+int N;
+
+void solve() {
+  VI res;
+  int bal=0,par=0;
+  REP(i,N) {
+    if(A[i]==0) res.push_back(A[i]),par^=1;
+    else {
+      bal+=(par==1?-1:1);
+      if(abs(bal)>1) {
+        assert(res.back()==0);
+        res.pop_back();
+        bal=0;
+      } else {
+        par^=1;
+      }
+      res.push_back(A[i]);
+    }
+  }
+  if(bal!=0) {
+    VI ans;
+    int p=-1;
+    REP(i,SZ(res)) if(res[i]==1) p=i;
+    assert(p!=-1);
+    REP(i,SZ(res)) if(i!=p) ans.push_back(res[i]);
+    //dumpc(res);
+    //dumpc(ans);
+    res=ans;
+  }
+  //dumpc(res);
+  assert(SZ(res)>=N/2);
+  cout<<SZ(res)<<endl;
+  REP(i,SZ(res)) cout<<res[i]<<" ";
+  cout<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+
+  int t; cin>>t;
+  while(t--) {
+    cin>>N;
+    REP(i,N) cin>>A[i];
+    solve();
+  }
+
+  return 0;
+}
