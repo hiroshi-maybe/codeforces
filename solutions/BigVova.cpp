6fc8bdc26516641f95fe4f4a8628478278c14f9c
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-09-10 22:07:17 -0700

    solve R669 div2 B. Big Vova

---
+++ solutions/BigVova.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch BigVova | diff BigVova.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address BigVova.cpp && ./a.out
+
+/*
+
+ 9/8/2020
+
+ 8:09-8:22 WA
+ 8:25 AC
+
+ https://codeforces.com/blog/entry/82417
+ https://twitter.com/hanseilimak/status/1304285236352614400
+
+ */
+
+const int MAX_N=1e6+1;
+int A[MAX_N];
+int N;
+
+int gcd(int a, int b) { return b==0?a:gcd(b,a%b); }
+void solve() {
+  sort(A,A+N),reverse(A,A+N);
+  VI res={A[0]};
+  int g=A[0];
+  VI viz(N,0); viz[0]=1;
+  REP(_,N-1) {
+    int maxg=0,p=-1;
+    REP(i,N) if(viz[i]==0) {
+      int gg=gcd(g,A[i]);
+      if(gg>maxg) maxg=gg,p=i;
+    }
+    assert(p!=-1);
+    viz[p]=1,res.push_back(A[p]);
+    g=maxg;
+  }
+  REP(i,N) cout<<res[i]<<" ";
+  cout<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+
+  int t; cin>>t;
+  while(t--) {
+    cin>>N;
+    REP(i,N) cin>>A[i];
+    solve();
+  }
+
+  return 0;
+}
