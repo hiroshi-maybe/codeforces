70d4eac7c929cc43d46abec04ecd8a17b869d1c0
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-11-02 22:52:05 -0800

    solve R681 div1 B. Identify the Operations

---
+++ solutions/IdentifyTheOperations.cpp
@@ -0,0 +1,235 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+template <typename Val>
+struct SegmentTree {
+  int N_; // adjusted N
+  vector<Val> tree;
+  Val id;
+  using Merge = function<Val(Val,Val)>;
+  Merge merge;
+public:
+  SegmentTree(int N, Val id, Merge merge) { prep(N,id,merge); }
+  SegmentTree(vector<Val> A, Val id, Merge merge) { prep(A.size(),id,merge), this->build(A); }
+  SegmentTree& prep(int N, Val id, Merge merge) {
+    this->id=id,this->merge=merge;
+    int n=1; while(n<N) n<<=1; // Init by power of 2
+    this->tree=vector<Val>(2*n-1,id), this->N_=n;
+    return *this;
+  }
+  void build(const vector<Val> &ns) {
+    for(int i=0; i<ns.size(); ++i) tree[i+N_-1]=ns[i];
+    for(int i=N_-2; i>=0; --i) mergeAt(i);
+  } // Initialize tree with `ns`
+  void update(int i, const Val &x) { i+=N_-1,tree[i]=x; while(i>0) i=(i-1)/2,tree[i]=mergeAt(i); } // Update k-th (0-indexed) value
+  Val query(int ql, int qr) { return queryTree(ql,qr,0,0,N_); } // query in range [ql,qr)
+private:
+  Val mergeAt(int i) { return tree[i]=merge(tree[2*i+1],tree[2*i+2]); }
+  Val queryTree(const int ql, const int qr, int i, int tl, int tr) {
+    if (tr<=ql||qr<=tl) return id; // out of range
+    if (ql<=tl&&tr<=qr) return tree[i]; // all covered
+    int mid=tl+(tr-tl)/2; // partially covered
+    return merge(queryTree(ql,qr,2*i+1, tl,mid),
+                 queryTree(ql,qr,2*i+2,mid, tr));
+  }
+};
+template<typename Val> auto makeRMQ(vector<Val> A, Val id) {
+  return SegmentTree<Val>(A,id,[](Val a, Val b) { return max(a,b); });
+}
+template<typename Val> auto makeRmQ(vector<Val> A, Val id) {
+  return SegmentTree<Val>(A,id,[](Val a, Val b) { return min(a,b); });
+}
+template <typename T> struct BIT {
+public:
+  int N;
+  vector<T> A;
+  BIT() {}
+  BIT(int N) { init(N); }
+  void init(int N) {
+    this->N=N;
+    A=vector<T>(N+1,0);
+  }
+  // query ∑ { sum[i] : i=l..<r }
+  T query(int l, int r) {
+    assert(l<=r&&0<=l&&r<=N);
+    return query(r)-query(l);
+  }
+  // min index s.t. dat[0..i]>x
+  int upperbound(T x) {
+    int good=N+1,bad=0;
+    while(abs(good-bad)>1) {
+      int m=(good+bad)/2;
+      (query(m)>x?good:bad)=m;
+    }
+    return good-1;
+  }
+  // sum[i]+=x
+  void add(int i, T x) {
+    assert(0<=i&&i<N);
+    ++i;
+    while(i<=N) A[i]+=x,i+=lsone(i);
+  }
+  // sum[i]=x
+  void update(int i, T v) {
+    T pre=query(i+1)-query(i);
+    add(i,v-pre);
+  }
+private:
+  int lsone(int i) { return i&-i; }
+  // query in [0,r) : ∑ { sum[i] : i=0..r-1 }
+  T query(int r) {
+    assert(0<=r&&r<=N);
+    T res=0;
+    while(r>0) res+=A[r],r-=lsone(r);
+    return res;
+  }
+};
+const int MOD=998244353;
+struct ModInt {
+  unsigned int val;
+  ModInt(): val(0) {}
+  ModInt(int v) { norm(v%MOD); }
+  ModInt(long long v) { norm(v%MOD); }
+  ModInt& norm(long long v) {
+    v=v<0?v%MOD+MOD:v; // negative
+    v=v>=MOD?v-MOD:v; // mod
+    val=(unsigned int)v;
+    return *this;
+  }
+  explicit operator bool() const { return val!=0; }
+  ModInt operator-() const { return ModInt(0)-*this; }
+  ModInt &operator+=(ModInt that) { return norm((long long)val+that.val); }
+  ModInt &operator-=(ModInt that) { return norm((long long)val-that.val); }
+  ModInt &operator*=(ModInt that) { val=(unsigned long long)val*that.val%MOD; return *this; }
+  ModInt &operator/=(ModInt that) { return *this*=that.inv(); }
+  ModInt operator+(ModInt that) const { return ModInt(*this)+=that; }
+  ModInt operator-(ModInt that) const { return ModInt(*this)-=that; }
+  ModInt operator*(ModInt that) const { return ModInt(*this)*=that; }
+  ModInt operator/(ModInt that) const { return ModInt(*this)/=that; }
+  ModInt pow(long long n) const {
+    ModInt x=*this, res=1;
+    while(n>0) {
+      if(n&1) res*=x;
+      x*=x,n>>=1;
+    }
+    return res;
+  }
+  ModInt inv() const { return (*this).pow(MOD-2); }
+  bool operator==(ModInt that) const { return val==that.val; }
+  bool operator!=(ModInt that) const { return val!=that.val; }
+  friend ostream& operator<<(ostream& os, const ModInt& that) { return os<<that.val; }
+};
+// $ cp-batch IdentifyTheOperations | diff IdentifyTheOperations.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address IdentifyTheOperations.cpp && ./a.out
+
+/*
+
+ 11/2/2020
+
+ 7:30-8:33 AC
+
+ https://twitter.com/hanseilimak/status/1323518002021634048
+
+ https://codeforces.com/blog/entry/84298
+
+ https://twitter.com/Cinnamon_VR/status/1323303362561892352
+ https://twitter.com/n_vip/status/1323303364722003968
+ https://twitter.com/ngtkana/status/1323303927920537600
+ https://twitter.com/laycrs/status/1323304091108343813
+ https://twitter.com/drken1215/status/1323304125564551168
+
+ */
+
+const int MAX_N=1e6+1;
+int A[MAX_N],B[MAX_N];
+int N,K;
+
+void solve() {
+  map<int,int> rtime;
+  REP(i,K) rtime[B[i]]=i;
+  VI ls(N),rs(N);
+  VI ps(N+1); REP(i,N) ps[A[i]]=i;
+
+  auto rmq=makeRMQ(VI(N+1,-1),-1);
+  REP(i,N) if(rtime.count(A[i])) {
+    int lp=rmq.query(rtime[A[i]],K);
+    ls[i]=lp;
+    rmq.update(rtime[A[i]],i);
+  }
+  rmq=makeRmQ(VI(N+1,N),N);
+  for(int i=N-1; i>=0; --i) if(rtime.count(A[i])) {
+    int rp=rmq.query(rtime[A[i]],K);
+    rs[i]=rp;
+    //dump(i,A[i],rp);
+    rmq.update(rtime[A[i]],i);
+  }
+  //dumpc(rs);
+
+  BIT<int> bit(N);
+  ModInt res=1;
+  REP(i,K) {
+    int p=ps[B[i]];
+    int lp=ls[p],lcnt=bit.query(lp+1,p),reml=p-lp-1-lcnt;
+    int rp=rs[p],rcnt=bit.query(p+1,rp),remr=rp-p-1-rcnt;
+    //dump(i,B[i],p,lp,lcnt,reml,rp,rcnt,remr);
+    res*=(reml>0)+(remr>0);
+    bit.add(p,1);
+  }
+  cout<<res<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+
+  int t; cin>>t;
+  while(t--) {
+    cin>>N>>K;
+    REP(i,N) cin>>A[i];
+    REP(i,K) cin>>B[i];
+    solve();
+  }
+
+  return 0;
+}
