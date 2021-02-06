809309f770cc366220ae1b2eea57e12d19709923
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-02-05 21:18:51 -0800

    solve R699 div2 A. Space Navigation

---
+++ solutions/SpaceNavigation.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch SpaceNavigation | diff SpaceNavigation.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address SpaceNavigation.cpp && ./a.out
+
+/*
+
+ 2/5/2021
+
+ 6:35-6:49 AC
+
+ https://codeforces.com/blog/entry/87523
+
+ */
+
+map<char,pair<int,int>> moves = { {'R',{0,1}}/*R*/,{'D',{1,0}}/*D*/,{'L',{0,-1}}/*L*/,{'U',{-1,0}}/*U*/ };
+string S;
+int px,py;
+
+bool solve() {
+  int lx=0,rx=0,ly=0,ry=0;
+  REP(i,SZ(S)) {
+    if(S[i]=='R') rx+=1;
+    if(S[i]=='D') ly-=1;
+    if(S[i]=='L') lx-=1;
+    if(S[i]=='U') ry+=1;
+  }
+  return lx<=px&&px<=rx&&ly<=py&&py<=ry;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+
+  int t; cin>>t;
+  while(t--) {
+    cin>>px>>py>>S;
+    cout<<(solve()?"YES":"NO")<<endl;
+  }
+
+  return 0;
+}
