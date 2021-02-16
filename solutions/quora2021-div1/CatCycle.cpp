758315870db4743cf32666e457e0506a9c8c98de
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-02-16 10:43:39 -0800

    solve E104 B. Cat Cycle

---
+++ solutions/quora2021-div1/CatCycle.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch CatCycle | diff CatCycle.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address CatCycle.cpp && ./a.out
+
+/*
+
+ 2/15/2021
+
+ 6:40-7:39 AC
+
+ https://codeforces.com/blog/entry/87873
+ https://twitter.com/hanseilimak/status/1361747940126023680
+
+ https://twitter.com/kiri8128/status/1361353767707316226
+ https://twitter.com/laycrs/status/1361353390647746560
+ https://twitter.com/kzyKT_M/status/1361353560647032836
+
+ */
+
+LL N,K;
+
+LL solve() {
+  if(N%2==0) {
+    return (K-1)%N;
+  }
+
+  LL a=(N-1)/2;
+  LL k=K-1;
+
+/*
+  LL x=k/a;
+  k-=x*a;
+  LL cur=x*(a+1)%N;
+  //dump(N,K,a,x,k,cur);
+  LL res=(cur+k)%N;
+  return res;*/
+
+  LL skip=k/a;
+  return (k+skip)%N;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+
+  int t; cin>>t;
+  while(t--) {
+    cin>>N>>K;
+    cout<<solve()+1<<endl;
+  }
+
+  return 0;
+}
