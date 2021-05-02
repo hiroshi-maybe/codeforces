4cc89adc789746c85335f9264b5c43826ffd383f
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-05-02 10:31:44 -0700

    Update c++ header search path after Big Sur upgrade

--- script/bin/link-bitsstdcpp.sh
+++ script/bin/link-bitsstdcpp.sh
@@ -1,2 +1,4 @@
 #!/bin/zsh
-sudo ln -s ${TCHOME}/include/bits /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1/
+sudo ln -s ${TCHOME}/include/bits /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/
+
+# Run `clang -x c -v -E /dev/null` to double check the header search path of clang
