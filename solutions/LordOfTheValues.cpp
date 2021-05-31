90f610daf20751ecdc19c719ab30ea73f1a2545a
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-05-31 12:30:18 -0700

    solve DS2021 div1+2 B. Lord of the Values

---
+++ solutions/LordOfTheValues.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch LordOfTheValues | diff LordOfTheValues.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address LordOfTheValues.cpp && ./a.out
+
+/*
+
+ 5/30/2021
+
+ 8:02-8:27 AC
+
+ https://codeforces.com/blog/entry/91271
+
+ */
+
+const int MAX_N=1e6+1;
+LL A[MAX_N];
+int N;
+
+void solve() {
+  vector<LL> as(A,A+N);
+
+  auto op1=[&](int i) {
+    cout<<"1 "<<i+1<<" "<<i+2<<endl;
+    as[i]+=as[i+1];
+  };
+  auto op2=[&](int i) {
+    cout<<"2 "<<i+1<<" "<<i+2<<endl;
+    as[i+1]-=as[i];
+  };
+  cout<<N/2*6<<endl;
+  for(int i=0; i<N; i+=2) {
+    op1(i);
+    REP(_,2) op2(i);
+    op1(i);
+    REP(_,2) op2(i);
+  }
+
+  REP(i,N) assert(as[i]==-A[i]);
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+
+  int t; cin>>t;
+  while(t--) {
+    cin>>N;
+    REP(i,N) cin>>A[i];
+    solve();
+  }
+
+  return 0;
+}
