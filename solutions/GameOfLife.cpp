e9fea501af481c4a56e19527e9d3775cfb72724c
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-05-31 12:28:44 -0700

    solve DS2021 div1+2 A. Game of Life

---
+++ solutions/GameOfLife.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// stdout
+#define println(args...) fprintf(stdout, ##args),putchar('\n');
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+// $ cp-batch GameOfLife | diff GameOfLife.out -
+// $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address GameOfLife.cpp && ./a.out
+
+/*
+
+ 5/30/2021
+
+ 7:35-8:00 AC
+
+ https://codeforces.com/blog/entry/91271
+
+ */
+
+string S;
+int N,M;
+
+void solve() {
+  string s(N+2,'0');
+  REP(i,N) s[i+1]=S[i];
+  int n=min(N,M);
+  while(n--) {
+    string ss=s;
+    FORE(i,1,N) if(s[i]=='0') {
+      if(s[i+1]=='1'&&s[i-1]=='0') ss[i]='1';
+      else if(s[i+1]=='0'&&s[i-1]=='1') ss[i]='1';
+    }
+    swap(s,ss);
+  }
+  string res(N,'0');
+  REP(i,N) res[i]=s[i+1];
+  cout<<res<<endl;
+}
+
+void solve_wa() {
+  map<int,int> es;
+  REP(i,N) if(S[i]=='1') {
+    es[i-M]++,es[i+M+1]--;
+  }
+  string res(N,'0');
+  int cur=0;
+  auto it=es.begin();
+  REP(i,N) {
+    if(it!=es.end()&&it->first<=i) {
+      cur+=it->second,++it;
+    }
+    if(cur>0) res[i]='1';
+  }
+  cout<<res<<endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout<<setprecision(12)<<fixed;
+
+  int t; cin>>t;
+  while(t--) {
+    cin>>N>>M>>S;
+    solve();
+  }
+
+  return 0;
+}
