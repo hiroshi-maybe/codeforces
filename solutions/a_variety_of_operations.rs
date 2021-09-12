d4eee5cb921c120a631976dd06fb47cc705c9aea
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-09-11 23:42:03 -0700

    solve Deltix Summer 2021 A. A Variety of Operations with Rust

---
+++ solutions/a_variety_of_operations.rs
@@ -0,0 +1,76 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr,) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => { ($(read_value!($next, $t)),*) };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+    ($next:expr, usize1) => (read_value!($next, usize) - 1);
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        read_value!($next, [$t; len])
+    }};
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+// $ rs-cp-batch a_variety_of_operations | diff a_variety_of_operations.out -
+// $ cargo run a_variety_of_operations
+
+///
+/// 9/11/2021
+///
+/// 23:16-23:34 AC
+///
+fn main() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {($($format:tt)*) => (let _ = write!(out,$($format)*););}
+    input! {
+        n: i32,
+        cd: [(i64, i64); n],
+    }
+    for (c, d) in cd {
+        let res = if c%2!=d%2 {
+            -1
+        } else if c==0&&d==0 {
+            0
+        } else if c==d {
+            1
+        } else {
+            2
+        };
+
+        puts!("{}\n", res);
+    }
+
+}
