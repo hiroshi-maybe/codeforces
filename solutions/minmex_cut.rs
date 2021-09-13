6723fdbe6128f5bf7e4db96571b0aa1a2b113c54
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-09-12 23:35:55 -0700

    solve GR16 div1+2 B. MIN-MEX Cut

---
+++ solutions/minmex_cut.rs
@@ -0,0 +1,75 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr,) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => { ($(read_value!($next, $t)),*) };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+    ($next:expr, usize1) => (read_value!($next, usize) - 1);
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        read_value!($next, [$t; len])
+    }};
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+// $ rs-cp-batch minmex_cut | diff minmex_cut.out -
+// $ cargo run minmex_cut
+
+///
+/// 9/12/2021
+///
+/// 7:55-8:10 AC
+///
+/// https://codeforces.com/blog/entry/94803
+///
+fn main() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {($($format:tt)*) => (let _ = write!(out,$($format)*););}
+    input! {
+        t: usize,
+        s: [chars; t],
+    }
+
+    for s in s {
+        let mut cnt=0;
+        for (i, c) in s.iter().enumerate() {
+            if *c=='0'&&(i==0||s[i-1]=='1') {
+                cnt+=1;
+            }
+        }
+        let res=min(cnt,2);
+        puts!("{}\n", res);
+    }
+}
