d33d1d33f2859fabd3141c6efae5a85995c335fb
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-10-08 00:10:48 -0700

    Add BIT implementation

---
+++ lib/cp-lib/src/bit.rs
@@ -0,0 +1,83 @@
+///
+/// Binary Indexed Tree (Fenwick tree)
+/// =======
+///
+/// # Use cases
+///
+/// * Point update in O(lg N)
+/// * Range sum query in O(lg N)
+///
+/// # Examples
+///
+/// ```
+/// // @TODO
+/// ```
+///
+/// # References:
+///
+/// * https://github.com/hiroshi-maybe/topcoder/blob/master/lib/range-query.cpp
+/// * https://cp-algorithms.com/data_structures/fenwick.html
+/// * https://en.wikipedia.org/wiki/Fenwick_tree
+/// * https://github.com/atcoder/ac-library/blob/master/document_ja/fenwicktree.md
+/// * https://github.com/rust-lang-ja/ac-library-rs/blob/master/src/fenwicktree.rs
+///
+/// # Used problems
+///
+/// *
+///
+
+// region: bit
+
+#[rustfmt::skip]
+#[allow(dead_code)]
+pub mod bit {
+    pub struct BIT<T> { n: usize, dat: Vec<T>, /* dat[i] = ∑ { A[g(i)..=i] } */ identity: T }
+    impl<T> BIT<T>
+    where
+        T: Clone + std::ops::AddAssign,
+    {
+        pub fn new(n: usize, identity: T) -> Self {
+            BIT { n, dat: vec![identity.clone(); n], identity }
+        }
+        // A[pos] += delta
+        pub fn add(&mut self, pos: usize, delta: T) {
+            let mut i = pos + 1;
+            while i <= self.n {
+                self.dat[i - 1] += delta.clone();
+                i = h(i);
+            }
+        }
+        // query ∑ { A[i] : i=r.start..<r.end }
+        pub fn query_range(&self, range: std::ops::Range<usize>) -> T
+        where
+            T: std::ops::Sub<Output = T>,
+        {
+            debug_assert!(range.start <= range.end);
+            self.query(..range.end) - self.query(..range.start)
+        }
+        // query ∑ { A[i] : i=0..<r.end }
+        pub fn query(&self, range: std::ops::RangeTo<usize>) -> T {
+            let mut r = range.end;
+            debug_assert!(r <= self.n);
+            let mut res = self.identity.clone();
+            while r > 0 {
+                res += self.dat[r - 1].clone();
+                r = std::cmp::max(g(r), 1) - 1;
+            }
+            res
+        }
+    }
+    fn g(i: usize) -> usize { i & (i + 1) }
+    fn h(i: usize) -> usize { i | (i + 1) }
+}
+pub use bit::BIT;
+
+// endregion: bit
+
+#[cfg(test)]
+mod tests_modint {
+    use super::*;
+
+    #[test]
+    fn test() {}
+}
