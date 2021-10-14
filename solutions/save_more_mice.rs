30520dce5acf744189a982e05941736d7b20e5aa
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-10-13 22:10:00 -0700

    solve R748 div3 C. Save More Mice

---
+++ solutions/save_more_mice.rs
@@ -0,0 +1,81 @@
+#![allow(unused_macros, unused_imports)]
+use std::cmp::*;
+use std::collections::*;
+
+#[rustfmt::skip]
+#[macro_use]
+#[allow(dead_code)]
+mod io {
+	macro_rules! with_dollar_sign { ($($body:tt)*) => { macro_rules! __with_dollar_sign { $($body)* } __with_dollar_sign!($); }}
+	macro_rules! setup_out {
+		($fn:ident,$fn_s:ident) => {
+			use std::io::Write;
+			let out = std::io::stdout();
+			let mut out = ::std::io::BufWriter::new(out.lock());
+			with_dollar_sign! { ($d:tt) => {
+				macro_rules! $fn { ($d($format:tt)*) => { let _ = write!(out,$d($format)*); } }
+				macro_rules! $fn_s { ($d($format:tt)*) => { let _ = writeln!(out,$d($format)*); } }
+			}}
+		};
+	}
+	macro_rules! _epr { ($v:expr $(,)?) => {eprint!("{} = {:?}, ", stringify!($v), $v)}; }
+	macro_rules! dbgln { ($($val:expr),*) => {{ eprint!("[{}:{}] ", file!(), line!()); ($(_epr!($val)),*); eprintln!(); }}; }
+	pub fn readln() -> String {
+		let mut line = String::new();
+		::std::io::stdin().read_line(&mut line).unwrap_or_else(|e| panic!("{}", e));
+		line
+	}
+	macro_rules! readlns {
+		($($t:tt),*; $n:expr) => {{ let stdin = ::std::io::stdin();
+			::std::io::BufRead::lines(stdin.lock()).take($n).map(|line| {
+				let line = line.unwrap(); #[allow(unused_mut)]let mut it = line.split_whitespace(); _read!(it; $($t),*)
+			}).collect::<Vec<_>>()
+		}};
+	}
+	macro_rules! readln {
+		($($t:tt),*) => {{ let line = io::readln(); #[allow(unused_mut)]let mut it = line.split_whitespace(); _read!(it; $($t),*) }};
+	}
+	macro_rules! _read {
+		($it:ident; [char]) => { _read!($it; String).chars().collect::<Vec<_>>() };
+		($it:ident; [u8]) => { Vec::from(_read!($it; String).into_bytes()) };
+		($it:ident; usize1) => { $it.next().unwrap_or_else(|| panic!("input mismatch")).parse::<usize>().unwrap_or_else(|e| panic!("{}", e)) - 1 };
+		($it:ident; [usize1]) => { $it.map(|s| s.parse::<usize>().unwrap_or_else(|e| panic!("{}", e)) - 1).collect::<Vec<_>>() };
+		($it:ident; [$t:ty]) => { $it.map(|s| s.parse::<$t>().unwrap_or_else(|e| panic!("{}", e))).collect::<Vec<_>>() };
+		($it:ident; $t:ty) => { $it.next().unwrap_or_else(|| panic!("input mismatch")).parse::<$t>().unwrap_or_else(|e| panic!("{}", e)) };
+		($it:ident; $($t:tt),+) => { ($(_read!($it; $t)),*) };
+	}
+}
+
+// $ rs-cp-batch save_more_mice | diff save_more_mice.out -
+// $ cargo run --bin save_more_mice
+
+///
+/// 10/13/2021
+///
+/// 8:17-8:42 AC
+///
+/// https://twitter.com/laycrs/status/1448330998245048320
+/// https://twitter.com/kzyKT_M/status/1448330955526230016
+///
+
+fn main() {
+    setup_out!(put, puts);
+
+    let t = readln!(usize);
+    for _ in 0..t {
+        let (n, k) = readln!(i64, usize);
+        let mut a = readln!([i64]);
+        a.sort_by_key(|&a| Reverse(a));
+        let mut cp = 0;
+        let mut res = 0;
+        for i in 0..k {
+            if cp < a[i] {
+                res += 1;
+            }
+            let dis = n - a[i];
+            cp += dis;
+        }
+
+        puts!("{}", res);
+    }
+}
