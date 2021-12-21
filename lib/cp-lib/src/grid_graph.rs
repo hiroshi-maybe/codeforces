3bfcf242d305f4171bc062e8c9a2f8d1054e3d4f
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-12-20 22:38:25 -0800

    Add adjacent two cells to grid graph library

--- lib/cp-lib/src/grid_graph.rs
+++ lib/cp-lib/src/grid_graph.rs
@@ -30,28 +30,33 @@
 #[allow(dead_code)]
 mod grid_graph {
     pub struct GridGraph { h: usize, w: usize, }
-    impl GridGraph {
-        pub fn new(h: usize, w: usize) -> GridGraph { GridGraph { h, w } }
-        pub fn adj_cells4(&self, i: usize, j: usize) -> AdjacentCells {
-            assert!(i < self.h && j < self.w);
-            AdjacentCells { cur: 0, i: i as i64, j: j as i64, w: self.w as i64, h: self.h as i64 }
-        }
-    }
-    pub struct AdjacentCells { cur: usize, i: i64, j: i64, w: i64, h: i64 }
-    impl Iterator for AdjacentCells {
-        type Item = (usize, usize);
-        fn next(&mut self) -> Option<Self::Item> {
-            const MOVES: [(i64, i64); 4] = [(1,0),(0,1),(-1,0),(0,-1)];
-            for &(di,dj) in MOVES[self.cur..].iter() {
-                self.cur += 1;
-                let (ii, jj) = (self.i +di, self.j + dj);
-                if (0..self.h).contains(&ii) && (0..self.w).contains(&jj) {
-                    return Some((ii as usize, jj as usize));
+    impl GridGraph { pub fn new(h: usize, w: usize) -> GridGraph { GridGraph { h, w } } }
+    macro_rules! impl_adjacent_cells {
+        ($method: ident, $iterator: ident, $moves: expr) => {
+            impl GridGraph {
+                pub fn $method(&self, i: usize, j: usize) -> $iterator {
+                    assert!(i < self.h && j < self.w);
+                    $iterator { cur: 0, i: i as i64, j: j as i64, w: self.w as i64, h: self.h as i64 }
                 }
             }
-            None
+            pub struct $iterator { cur: usize, i: i64, j: i64, w: i64, h: i64 }
+            impl Iterator for $iterator {
+                type Item = (usize, usize);
+                fn next(&mut self) -> Option<Self::Item> {
+                    for &(di,dj) in $moves[self.cur..].iter() {
+                        self.cur += 1;
+                        let (ii, jj) = (self.i + di, self.j + dj);
+                        if (0..self.h).contains(&ii) && (0..self.w).contains(&jj) {
+                            return Some((ii as usize, jj as usize));
+                        }
+                    }
+                    None
+                }
+            }
         }
     }
+    impl_adjacent_cells!(adj_cells4, AdjacentCells4, vec![(1,0),(0,1),(-1,0),(0,-1)]);
+    impl_adjacent_cells!(adj_cells2, AdjacentCells2, vec![(1,0),(0,1)]);
 }
 pub use grid_graph::GridGraph;
 
@@ -74,6 +79,21 @@ mod tests_grid_graph {
 
         let g = GridGraph::new(1, 1);
         assert_eq!(g.adj_cells4(0, 0).collect::<Vec<_>>(), vec![]);
+    }
+
+    #[test]
+    fn test_adj_cells2() {
+        let g = GridGraph::new(10, 10);
+
+        assert_eq!(
+            g.adj_cells2(1, 1).collect::<Vec<_>>(),
+            vec![(2, 1), (1, 2)]
+        );
+
+        assert_eq!(g.adj_cells2(9, 9).collect::<Vec<_>>(), vec![]);
+
+        let g = GridGraph::new(1, 1);
+        assert_eq!(g.adj_cells2(0, 0).collect::<Vec<_>>(), vec![]);
     }
 
     #[test]
