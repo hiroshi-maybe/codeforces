dd0802a2d526c91b882ec3304c540de8eff50b85
Hiroshi Kori <hiroshi.maybe@gmail.com>
2022-06-05 23:58:21 -0700

    format segtree library

--- lib/cp-lib/src/segtree.rs
+++ lib/cp-lib/src/segtree.rs
@@ -24,26 +24,26 @@
 /// # References:
 /// * https://github.com/hiroshi-maybe/topcoder/blob/a20169f8ffdd463b83a9aa28a8c1ba9c2b029baa/lib/range-query.cpp#L51
 ///
+/// # Used problems:
+/// * https://github.com/hiroshi-maybe/leetcode/blob/master/307-Range-Sum-Query/range-sum-query-mutable.rs#L36
+///
 
 // region: segment_tree
 
+#[rustfmt::skip]
 #[allow(dead_code)]
 mod segment_tree {
     pub struct Min(i64);
     impl Monoid for Min {
         type V = i64;
         const IDENTITY: i64 = i64::max_value();
-        fn binary_operation(a: &i64, b: &i64) -> i64 {
-            *a.min(b)
-        }
+        fn binary_operation(a: &i64, b: &i64) -> i64 { *a.min(b) }
     }
     pub struct Max(i64);
     impl Monoid for Max {
         type V = i64;
         const IDENTITY: i64 = i64::min_value();
-        fn binary_operation(a: &i64, b: &i64) -> i64 {
-            *a.max(b)
-        }
+        fn binary_operation(a: &i64, b: &i64) -> i64 { *a.max(b) }
     }
     pub type RangeMinQuery = SegmentTree<Min>;
     pub type RangeMaxQuery = SegmentTree<Max>;
@@ -54,12 +54,7 @@ mod segment_tree {
         fn binary_operation(a: &Self::V, b: &Self::V) -> Self::V;
     }
 
-    pub struct SegmentTree<M: Monoid> {
-        n: usize,
-        size: usize,
-        dat: Vec<M::V>,
-    }
-
+    pub struct SegmentTree<M: Monoid> { n: usize, size: usize, dat: Vec<M::V>, }
     impl<M: Monoid> SegmentTree<M> {
         pub fn new(n: usize) -> SegmentTree<M> {
             let size = n.next_power_of_two();
@@ -72,9 +67,7 @@ mod segment_tree {
         pub fn query(&self, range: std::ops::Range<usize>) -> M::V {
             assert!(range.start <= range.end && range.end <= self.n);
             let (mut l, mut r) = (self.map_index(range.start), self.map_index(range.end));
-            if l == r {
-                return M::IDENTITY.clone();
-            }
+            if l == r { return M::IDENTITY.clone(); }
             let (mut lval, mut rval) = (M::IDENTITY.clone(), M::IDENTITY.clone());
             while l < r {
                 if l & 1 != 0 {
@@ -85,8 +78,7 @@ mod segment_tree {
                     r -= 1;
                     rval = M::binary_operation(&self.dat[r], &rval);
                 }
-                l >>= 1;
-                r >>= 1;
+                l >>= 1; r >>= 1;
             }
             M::binary_operation(&lval, &rval)
         }
@@ -95,32 +87,22 @@ mod segment_tree {
             let mut i = self.map_index(i);
             self.dat[i] = val;
             i >>= 1;
-            while i > 0 {
-                self.merge(i);
-                i >>= 1;
-            }
+            while i > 0 { self.merge(i); i >>= 1; }
         }
         pub fn max_right<Pred: Fn(&M::V) -> bool>(&self, l: usize, pred: Pred) -> usize {
             assert!(l <= self.n);
             assert!(pred(&M::IDENTITY.clone()));
-            if l == self.n {
-                return self.n;
-            }
+            if l == self.n { return self.n; }
             let mut l = self.map_index(l);
             let mut sum = M::IDENTITY.clone();
             while {
                 // do
-                while l % 2 == 0 {
-                    l >>= 1;
-                }
+                while l % 2 == 0 { l >>= 1; }
                 if !pred(&M::binary_operation(&sum, &self.dat[l])) {
                     while l < self.size {
                         l = 2 * l;
                         let res = M::binary_operation(&sum, &self.dat[l]);
-                        if pred(&res) {
-                            sum = res;
-                            l += 1;
-                        }
+                        if pred(&res) { sum = res; l += 1; }
                     }
                     return l - self.size;
                 }
@@ -134,25 +116,18 @@ mod segment_tree {
         pub fn min_left<Pred: Fn(&M::V) -> bool>(&self, r: usize, pred: Pred) -> usize {
             assert!(r <= self.n);
             assert!(pred(&M::IDENTITY.clone()));
-            if r == 0 {
-                return 0;
-            }
+            if r == 0 { return 0; }
             let mut r = self.map_index(r);
             let mut sum = M::IDENTITY.clone();
             while {
                 // do
                 r -= 1;
-                while r > 1 && r % 2 == 1 {
-                    r >>= 1;
-                }
+                while r > 1 && r % 2 == 1 { r >>= 1; }
                 if !pred(&M::binary_operation(&self.dat[r], &sum)) {
                     while r < self.size {
                         r = 2 * r + 1;
                         let res = M::binary_operation(&self.dat[r], &sum);
-                        if pred(&res) {
-                            sum = res;
-                            r -= 1;
-                        }
+                        if pred(&res) { sum = res; r -= 1; }
                     }
                     return r + 1 - self.size;
                 }
@@ -165,9 +140,7 @@ mod segment_tree {
         fn merge(&mut self, i: usize) {
             self.dat[i] = M::binary_operation(&self.dat[2 * i], &self.dat[2 * i + 1]);
         }
-        fn map_index(&self, i: usize) -> usize {
-            i + self.size
-        }
+        fn map_index(&self, i: usize) -> usize { i + self.size }
     }
 
     impl<M: Monoid> From<&[M::V]> for SegmentTree<M> {
@@ -179,16 +152,15 @@ mod segment_tree {
         }
     }
     impl<M: Monoid> From<Vec<M::V>> for SegmentTree<M> {
-        fn from(v: Vec<M::V>) -> Self {
-            v[..].into()
-        }
+        fn from(v: Vec<M::V>) -> Self { v[..].into() }
     }
 }
-
 pub use segment_tree::{Monoid, RangeMaxQuery, RangeMinQuery, SegmentTree};
 
+// endregion: segment_tree
+
 #[cfg(test)]
-mod tests_segement_tree {
+mod tests_segment_tree {
     use super::*;
 
     #[test]
